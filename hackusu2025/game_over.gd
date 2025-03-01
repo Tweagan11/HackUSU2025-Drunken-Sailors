d619efe9673bd9b1b791d32a07462e88e993d417
dpanc <dpanchekha@gmail.com>
2025-03-01 13:41:45 -0700

    final game

---
+++ hackusu2025/game_over.gd
@@ -0,0 +1,12 @@
+extends Control
+
+@onready var water_sound: AudioStreamPlayer2D = $water_sounds
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	water_sound.play() # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
