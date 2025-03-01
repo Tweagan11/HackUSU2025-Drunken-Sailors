a54a7de0c694a27cb1b3eabe5f9c3368a0989105
dpanc <dpanchekha@gmail.com>
2025-03-01 03:04:39 -0700

    drawing rocks done

--- hackusu2025/main.gd
+++ hackusu2025/main.gd
@@ -1,10 +1,17 @@
 extends Node
 
 @export var rock_scene: PackedScene
+@export var rock_scene2: PackedScene
+@export var rock_scene3: PackedScene
+@export var rock_scene4: PackedScene
+@export var rock_scene5: PackedScene
+@export var rock_scene6: PackedScene
+var rock_list = []
 @onready var audio_stream_player_2d: AudioStreamPlayer2D = $AudioStreamPlayer2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	rock_list = [rock_scene, rock_scene2, rock_scene3, rock_scene4, rock_scene5, rock_scene6]
 	spawn_rock()
 	audio_stream_player_2d.play()
 
@@ -14,9 +21,8 @@ func _process(delta: float) -> void:
 	pass
 
 func spawn_rock() :
-	var rock = rock_scene.instantiate()
+	var rock = rock_list.pick_random().instantiate()
 	add_child(rock)
-
 
 func _on_timer_timeout() -> void:
 	spawn_rock() # Replace with function body.
