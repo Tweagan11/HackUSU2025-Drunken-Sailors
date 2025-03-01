0c8821cbbe40a66d0868a38b306aeb0289b36067
dpanc <dpanchekha@gmail.com>
2025-03-01 12:39:36 -0700

    Changed hearts code

---
+++ hackusu2025/hearts.gd
@@ -0,0 +1,14 @@
+extends Sprite2D
+
+var hearts_4 = preload("res://Sprites/3_hearts.png")
+var hearts_3 = preload("res://Sprites/3_hearts.png")
+var hearts_2 = preload("res://Sprites/2_hearts.png")
+var hearts_1 = preload("res://Sprites/1_heart.png")
+var hearts_0 = preload("res://Sprites/0_hearts.png")
+
+var hearts = [hearts_4, hearts_3, hearts_2, hearts_1, hearts_0]
+var index = 0
+
+func _on_ship_hit() -> void:
+	index += 1
+	texture = hearts[index]
